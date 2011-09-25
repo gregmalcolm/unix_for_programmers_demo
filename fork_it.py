8793287d319b5051f3e33d1cfe4da34142560e39
gregmalcolm <greg@gregmalcolm.com>
2011-09-25 18:42:21 -0400

    Created Python version of keynote slideshow

---
+++ fork_it.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+def fork_it():
+    print "Parent pid is {0}".format(os.getpid())
+    if (not os.fork()):
+        print "In child process. Pid is now {0}".format(os.getpid())
+        sys.exit(42)
+
+    child_pid, status = os.wait()
+    exit_status = status >> 8 # Keep high byte only
+    print "Child (pid {0}) terminated with status {1}" \
+        .format(child_pid, exit_status)
+
+if __name__ == "__main__":
+  fork_it()
+
+
